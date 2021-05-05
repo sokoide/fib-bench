f5b9559b43f44b9edb339e90868b7e4cb9f96d26
So Koide <sokoide@gmail.com>
2021-05-05 11:20:01 +0900

    Add .NET

---
+++ net/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace foo
+{
+    class Program
+    {
+        static uint fib(uint i)
+        {
+            if (i <= 1) return i;
+            return fib(i - 1) + fib(i - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            for (uint i = 40; i < 44; i++)
+            {
+                sw.Reset();
+                sw.Start();
+                uint r = fib(i);
+                sw.Stop();
+                long t = sw.ElapsedMilliseconds;
+                Console.WriteLine($"{t}, fib({i}) = {r}");
+            }
+        }
+    }
+}
