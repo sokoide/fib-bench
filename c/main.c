56f7b069db1f903bfb0205367fccf43017afe48d
So Koide <sokoide@gmail.com>
2021-05-05 13:22:31 +0900

    Add C

---
+++ c/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+uint64_t fib(uint64_t i) {
+    if (i <= 1)
+        return i;
+    return fib(i - 1) + fib(i - 2);
+}
+
+int main(int argc, char** argv) {
+    uint64_t i;
+    for (i = 40; i < 44; i++) {
+        struct timespec tw1, tw2;
+        clock_gettime(CLOCK_MONOTONIC, &tw1);
+        uint64_t result = fib(i);
+        clock_gettime(CLOCK_MONOTONIC, &tw2);
+
+        double wall = 1000.0 * tw2.tv_sec + 1e-6 * tw2.tv_nsec -
+                      (1000.0 * tw1.tv_sec + 1e-6 * tw1.tv_nsec);
+        printf("%lf, fib(%llu)=%llu\n", wall, i, result);
+    }
+    return 0;
+}
