0b76c0b102959e253adea2f1d1bd3b927524e968
So Koide <sokoide@gmail.cojm>
2024-06-30 14:38:55 +0900

    refactor: ts

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -5,8 +5,7 @@ function fib(a: number): number {
 
 
 
-var i:number
-for(i=40;i<=43;i+=1) {
+for(let i:number=40;i<=43;i++) {
 	var st:number = performance.now()
 	var n:number = fib(i)
 	var ed:number = performance.now()
